225be69124ad20ac76f983aa373a785de7b9d0bd
Dano McKenzie <danomckenzie@gmail.com>
2022-10-11 16:04:03 -0600

    all components added interfaces services database

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { DatabaseJsonComponent } from './database.json/database.json.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { InterfacesComponent } from './interfaces/interfaces.component';
+import { NavComponent } from './nav/nav.component';
+import { EventsComponent } from './events/events.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DatabaseJsonComponent,
+    LoginComponent,
+    RegistrationComponent,
+    InterfacesComponent,
+    NavComponent,
+    EventsComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
